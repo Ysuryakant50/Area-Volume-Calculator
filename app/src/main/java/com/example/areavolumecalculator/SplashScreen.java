6ecd97e3d9caae2e67a40ec5a8cb7e362cbb519c
Suryakant Yadav <ysuryakant2550@gmail.com>
2024-07-30 17:19:36 +0530

    add splash screen

--- app/src/main/java/com/example/areavolumecalculator/SplashScreen.java
+++ app/src/main/java/com/example/areavolumecalculator/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.areavolumecalculator;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
 
+        Intent ihome = new Intent(SplashScreen.this, MainActivity.class);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(ihome);
+                finish();
+            }
+        },2500);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
